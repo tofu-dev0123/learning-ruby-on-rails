6204b9f078da5410bc34d32ce2259b534d92c4bb
tofu-dev0123 <m.komukai0123@gmail.com>
2026-03-02 23:09:15 +0900

    viewの作成

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,18 +1,19 @@
 class HelloController < ApplicationController
-  def index
-    if params[:msg] != nil then
-      msg = "Hello, " + params[:msg] + "!"
-    else
-      msg = "this is sample page."
-    end
+  skip_before_action :verify_authenticity_token
 
-    html = "
-    <html>
-    <body>
-      <h1>Sample Page</h1>
-      <p>" + msg + "</p>
-    </body>
-    </html>"
-    render html: html.html_safe
+  def index
+    if request.post?
+      @title = "Result"
+      @msg = "you typed: #{params[:input1]}."
+      @value = params[:input1]
+      # POST後にリダイレクト（クエリパラメータで値を保持）
+      redirect_to action: :index, input1: params[:input1],
+        title: @title, msg: @msg
+    else
+      # パラメータがない場合はデフォルト値を設定
+      @title = params[:title] || "Index"
+      @msg = params[:msg] || "type text..."
+      @value = params[:input1] || ""
+    end
   end
 end
