b9886d3734e4d25eb1255d21051a027fd1367279
tofu-dev0123 <m.komukai0123@gmail.com>
2026-03-04 13:27:16 +0900

    sessionの利用

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -6,7 +6,19 @@ class HelloController < ApplicationController
   def index
     @header = "layout sample"
     @footer = "copyright MASATO-KOMUKAI 2026."
-    @title = "New Layout"
-    @msg = "this is sample page!"
+    @title = "Session sample"
+
+    if request.post?
+      if params[:name].blank?
+        flash[:notice] = "Please input your name!"
+      else
+        @msg = "Hello #{params[:name]}!"
+        flash[:notice] = "登録が完了しました！"
+      end
+
+      redirect_to action: "index", title: @title, msg: @msg
+    else
+      @msg = params[:msg] ? params[:msg] : "Sample flash."
+    end
   end
 end
