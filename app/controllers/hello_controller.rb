b62c5bc9192a89d2e9756305fa7735574abfa9e6
tofu-dev0123 <m.komukai0123@gmail.com>
2026-03-04 12:45:07 +0900

    レイアウトの実装

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,19 +1,12 @@
 class HelloController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  layout "hello"
+
   def index
-    if request.post?
-      @title = "Result"
-      @msg = "you typed: #{params[:input1]}."
-      @value = params[:input1]
-      # POST後にリダイレクト（クエリパラメータで値を保持）
-      redirect_to action: :index, input1: params[:input1],
-        title: @title, msg: @msg
-    else
-      # パラメータがない場合はデフォルト値を設定
-      @title = params[:title] || "Index"
-      @msg = params[:msg] || "type text..."
-      @value = params[:input1] || ""
-    end
+    @header = "layout sample"
+    @footer = "copyright MASATO-KOMUKAI 2026."
+    @title = "New Layout"
+    @msg = "this is sample page!"
   end
 end
